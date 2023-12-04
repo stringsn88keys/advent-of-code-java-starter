00dee5e184e2dcf1d2cfbf8494094cf9c8b48aa9
Thomas Powell <twilliampowell@gmail.com>
2023-12-04 17:48:42 -0500

    Game Breakdown structure
    Signed-off-by: Thomas Powell <twilliampowell@gmail.com>

--- src/main/java/aoc/day02/Day02.java
+++ src/main/java/aoc/day02/Day02.java
@@ -5,10 +5,22 @@ import aoc.Day;
 import java.util.List;
 
 public class Day02 implements Day {
+    static GameBreakdown limits = null;
+
+    public static GameBreakdown limits() {
+        if(limits != null) return limits;
+        limits = new GameBreakdown("12 red, 13 green, 14 blue");
+        return limits;
+    }
 
     @Override
     public String part1(List<String> input) {
-        return input.isEmpty() ? "" : input.get(0);
+        return input
+                .stream()
+                .map((inputLine) -> new GameBreakdown(inputLine))
+                .reduce(0, (sum, gameBreakdown) -> { return sum + (gameBreakdown.lessThan(limits()) ? gameBreakdown.id() : 0); }, (a,b) -> a + b)
+                .toString();
+
     }
 
     @Override
---
+++ src/main/java/aoc/day02/GameBreakdown.java
@@ -0,0 +1,15 @@
+package aoc.day02;
+
+public class GameBreakdown {
+    GameBreakdown(String definition) {
+    }
+
+    public boolean lessThan(GameBreakdown other) {
+        return false;
+    }
+
+    public int id() {
+        return 0;
+    }
+
+}
